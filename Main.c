1b62ab7883d3a2f0146053fbf586aa9840327de6
Emmy Coatney <johncoantey@gmail.com>
2024-07-09 14:02:39 -0500

    Add placement verification

--- Main.c
+++ Main.c
@@ -14,10 +14,12 @@ enum Ships{
 };
 
 const int ship_sizes[5] = {5, 4, 3, 3, 2};
+const char ship_chars[5] = {'C', 'B', 'D', 'S', 'P'};
 
 typedef struct {
     bool hasBoat;
     bool isHit;
+    char shipType;
 } Tile;
 
 typedef struct {
@@ -38,11 +40,12 @@ void inititalize_board(Tile board[BOARD_SIZE][BOARD_SIZE]) {
         for (int j = 0; j < BOARD_SIZE; j++) {
             board[i][j].hasBoat = false;
             board[i][j].isHit = false;
+            board[i][j].shipType = '\0';
         }
     }
 }
 
-void place_ship(Player* player, int x, int y, int ship_size, char direction) {
+void place_ship(Player* player, int x, int y, char direction, int ship_size, int ship_type) {
     Ship* ship = &player->ships[0];
     ship->x = x;
     ship->y = y;
@@ -56,6 +59,8 @@ void place_ship(Player* player, int x, int y, int ship_size, char direction) {
 
         ship->tiles[i] = &player->board[x][y];
         player->board[x][y].hasBoat = true;
+        player->board[x][y].shipType = ship_type;
+        
     }
 }
 
@@ -67,7 +72,7 @@ void print_board(Tile board[BOARD_SIZE][BOARD_SIZE]) {
         printf("%c│", i + 65);
         for (int j = 0; j < BOARD_SIZE; j++) {
             if (board[i][j].hasBoat) {
-                printf("B");
+                printf("%c", board[i][j].shipType);
             } else {
                 printf("-");
             }
@@ -80,33 +85,78 @@ void print_board(Tile board[BOARD_SIZE][BOARD_SIZE]) {
     printf(" └───────────────────┘\n");
 }
 
-void get_user_input(int* x, int* y, char* direction) {
+bool validate_placement(Player* player, int x, int y, char direction, int ship_size) {
+    
+    switch (direction)
+    {
+    case 'V':
+        if (x + ship_size > BOARD_SIZE) {
+            return false;
+        }
+        
+        for (int i = 0; i < ship_size; i++) {
+            if (player->board[x + i][y].hasBoat) {
+                return false;
+            }
+        }
+
+        break;
+    
+    case 'H':
+        if (y + ship_size > BOARD_SIZE) {
+            return false;
+        }
+
+        for (int i = 0; i < ship_size; i++) {
+            if (player->board[x][y + i].hasBoat) {
+                return false;
+            }
+        }
+        
+        break;
+
+    default:
+        break;
+    }
+
+    return true;
+}
+
+void get_user_input(Player* player, int* x, int* y, char* direction, int ship_size) {
     do {
         char c;
         int i;
-
-        printf("Enter A letter and a number to place the start of your ship (e.g. A0): ");
-        if (scanf(" %c%d", &c, &i) != 2 || c < 'A' || c > 'J' || i < 0 || i > 9) {
-            printf("Invalid input. Please try again.\n");
-            while (getchar() != '\n'); // Clear input buffer
-        } else {
-            *x = c - 'A'; // Convert letter to integer
-            *y = i;
-            break; // Exit the loop if input is valid
-        }
-    } while (1);
-
-    do {
         char dir;
 
-        printf("Enter direction (H or V) for the ship: ");
-        if (scanf(" %c", &dir) != 1 || (dir != 'H' && dir != 'V')) {
-            printf("Invalid input. Please try again.\n");
-            while (getchar() != '\n'); // Clear input buffer
-        } else {
+        do {
+            printf("Enter A letter and a number to place the start of your ship (e.g. A0): ");
+            if (scanf(" %c%d", &c, &i) != 2 || c < 'A' || c > 'J' || i < 0 || i > 9) {
+                printf("Invalid input. Please try again.\n");
+                while (getchar() != '\n'); // Clear input buffer
+            } else {
+                break; // Exit the loop if input is valid
+            }
+        } while (1);
+
+        do {
+            printf("Enter direction (H or V) for the ship: ");
+            if (scanf(" %c", &dir) != 1 || (dir != 'H' && dir != 'V')) {
+                printf("Invalid input. Please try again.\n");
+                while (getchar() != '\n'); // Clear input buffer
+            } else {
+                break; // Exit the loop if input is valid
+            }
+        } while (1);
+
+        if (validate_placement(player, c - 'A', i, dir, ship_size)) {
+            *x = c - 'A'; // Convert letter to integer
+            *y = i;
             *direction = dir;
-            break; // Exit the loop if input is valid
+            break;
+        } else {
+            printf("Invalid placement. Please try again.\n");
         }
+
     } while (1);
 }
 
@@ -123,15 +173,15 @@ int main() {
         print_board(player1.board);
         int x, y;
         char direction;
-        get_user_input(&x, &y, &direction);
-        place_ship(&player1, x, y, ship_sizes[i], direction);
+        get_user_input(&player1, &x, &y, &direction, ship_sizes[i]);
+        place_ship(&player1, x, y, direction, ship_sizes[i], ship_chars[i]);
     }
 
     for (int i = 0; i < NUM_SHIPS_PER_PLAYER; i++) {
         int x, y;
         char direction;
-        get_user_input(&x, &y, &direction);
-        place_ship(&player2, x, y, ship_sizes[i], direction);
+        get_user_input(&player2, &x, &y, &direction, ship_sizes[i]);
+        place_ship(&player2, x, y, direction, ship_sizes[i], ship_chars[i]);
     }
     
     // Print the board
